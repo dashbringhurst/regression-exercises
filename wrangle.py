a3428b810976a552950883e7d1a0e6346f97ecd2
Dashiell Bringhurst <dashbringhurst@gmail.com>
2022-08-30 15:22:01 -0500

    unfinished

--- wrangle.py
+++ wrangle.py
@@ -12,7 +12,8 @@ def get_connection(db, user=env.user, host=env.host, password=env.password):
 def new_zillow_db():
     '''The function uses the get_connection function to connect to a database and retrieve the zillow dataset'''
     return pd.read_sql('''SELECT bedroomcnt, bathroomcnt, calculatedfinishedsquarefeet, taxvaluedollarcnt, 
-    yearbuilt, taxamount, fips from properties_2017;''', get_connection('zillow'))
+    yearbuilt, taxamount, fips from properties_2017 LEFT JOIN propertylandusetype USING(propertylandusetypeid)
+    WHERE propertylandusedesc IN ("Single Family Residential", "Inferred Single Family Residential");''', get_connection('zillow'))
 
 def get_zillow_data():
     '''
@@ -20,15 +21,11 @@ def get_zillow_data():
     a csv file if a local file does not exist, and returns a df.
     '''
     if os.path.isfile('zillow.csv'):
-        
         # If csv file exists read in data from csv file.
-        df = pd.read_csv('zillow.csv', index_col=0)
-        
-    else:
-        
+        df = pd.read_csv('zillow.csv', index_col=0)     
+    else:   
         # Read fresh data from db into a DataFrame
         df = new_zillow_db()
-        
         # Cache data
         df.to_csv('zillow.csv')
         
@@ -36,7 +33,8 @@ def get_zillow_data():
 
 def wrangle_zillow():
     '''This function acquires the zillow dataset from the Codeup database using a SQL query and returns a cleaned
-    dataframe from a csv file.'''
+    dataframe from a csv file. Observations with null values are dropped and column names are changed for
+    readability. Values expected as integers are converted to integer types (year, bedrooms, fips).'''
     # use the get_zillow_data function to acquire the dataset and save it to a csv
     df = get_zillow_data()
     # drop rows with null values
@@ -57,8 +55,11 @@ def split_data(df, column):
         column that will be used to stratify the train_test_split. The function returns three dataframes, a 
         training dataframe with 60 percent of the data, a validate dataframe with 20 percent of the data and test
         dataframe with 20 percent of the data.'''
+    # split the dataset into two, with 80 percent of the observations in train and 20 percent in test
     train, test = train_test_split(df, test_size=.2, random_state=217, stratify=df[column])
+    # split the train again into two sets, using a 75/25 percent split
     train, validate = train_test_split(train, test_size=.25, random_state=217, stratify=train[column])
+    # return three datasets, train (60 percent of total), validate (20 percent of total), and test (20 percent of total)
     return train, validate, test
 
 def quantile_scaler():
@@ -68,9 +69,9 @@ def quantile_scaler():
     # scale the train set and save into a variable as a dataframe
     X_train = pd.DataFrame(scaler.fit_transform(train))
     # scale the validate set and save into a variable as a dataframe
-    X_validate = pd.DataFrame(scaler.fit_transform(validate))
+    X_validate = pd.DataFrame(scaler.transform(validate))
     # scale the test set and save into a variable as a dataframe
-    X_test = pd.DataFrame(scaler.fit_transform(test))
+    X_test = pd.DataFrame(scaler.transform(test))
     # return the three scaled dataframes for data exploration and modeling
     return X_train, X_validate, X_test
 
